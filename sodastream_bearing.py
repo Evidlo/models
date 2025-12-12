a306bf25b8f07003f85ca6deafe752973a23bb92
Evidlo <evan+gh@evanw.org>
2025-12-12 12:16:22 -0600

    init commit

---
+++ sodastream_bearing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Endcaps for binoculars
+
+import cadquery as cq
+from cadquery.vis import show
+
+base_height = 1
+wall = 2
+diam = 27.2
+height = 5
+
+result = (
+    cq.Workplane('front')
+    .circle((diam + wall)/2).extrude(base_height)
+    .workplane().circle((diam + wall)/2).circle(diam/2).extrude(height)
+)
+
+# Render the solid
+show(result)
+cq.exporters.export(result, 'sodastream_bearing.stl')
